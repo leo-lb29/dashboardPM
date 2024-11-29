727cef719a8ed60e8d8b1729d7532ba596923e10
Project Maker <postyourbot@gmail.com>
2024-11-29 09:54:20 +0100

    a

---
+++ phys.py
@@ -0,0 +1,136 @@
+---
+On appelle tension electrique cette difference de potentiel.
+UAB = VA - VB
+
+---
+P = u * i = > P = (V_A-V_B)×i
+
+
+P = u * i > 0 = >  RECEPTEUR
+
+P = u * i < 0 = >  GENERATEUR
+
+L energie dissipee dans le dipole est definie par
+E = P*t
+
+E > 0: Le dipole reçoit de l energie: c est un RECEPTEUR.
+
+E < 0: Le dipole cède de l energie: c est un GENERATEUR.
+
+
+Resistance en derivation
+i = u/R_eq = (1/R_eq)*u
+
+1/R_eq
+
+Resistance en serie
+u_AB = (R_eq)*i
+
+R_eq
+
+Generateur de tension reel
+
+1.
+
+e <=
+i = >
+Uab <=
+
+u_AB = e+ri
+
+2.
+
+e <=
+i <=
+Uab <=
+
+u_AB = +e-ri
+
+
+3.
+
+e = >
+i = >
+Uab <=
+
+u_AB = -e+ri
+
+
+4.
+
+e = >
+i <=
+Uab <=
+
+u_AB = -e-ri
+
+Loi des noeuds
+I1 + I2 = I3 Ou bien   I1 + I2 - I3 = 0
++  pour les courants qui arrivent au nœud N
+-  Pour les courants qui repartent du nœud N
+
+Loi des mailes
+SUM 1(±u) = 0)
+
++:   tensions dirigees dans le sens de parcours de la maille.
+-: tensions dirigees dans le sens inverse du sens de parcours de la maille.
+
+
+La tension efficace est liee à la tension maximale par
+U_eff= U_max / RACINE(2)
+
+
+omega = 2 pi f = 2 pi/T
+𝜔 est exprimee en rad.s-1
+
+
+s(t) = S_m  sin(omega t + fi) ou bien s(t) = S_m cos(omega t + fi)
+
+NB COMPLEXE
+
+Un nombre complexe peut s ecrire sous la forme algebrique:   _z = a+jb
+
+ou bien sous la forme trigonometrique: _z = p(cos fi + j sin fi)
+
+ou bien encore sous forme exponentielle:   _z = p e ^ j fi
+
+
+le module  est:
+| _z |= p= RACINE(a ^ 2 + b ^ 2)
+
+Argument est:
+arg(_z)= fi
+
+
+avec tan(fi = b/a)
+donc
+fi = arc tan(b/a)  si  a > 0
+
+
+
+
+
+Loi d Ohm: _U= _Z  _I
+
+avec _Z = Z e ^ jfi   et   fi = arg⁡▁Z
+
+Resistance _(Z_R) = R
+
+
+Condensateur _(Z_C)= 1/jComega = -j/Comega
+
+
+
+Bobine _(Z_L) = j Lomega
+
+
+
+Loi d ASSOCIATIONS
+
+En serie
+
+_Z_eq
+
+En derivation/Parrallèle
+
+1/_Z_eq
